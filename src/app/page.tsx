1a0422fa61816b74bb470e2771c9fafe76aab44e
Abu Shyed Hemel <50671938+AbuShyedHemel@users.noreply.github.com>
2023-10-29 17:13:17 +0600

    linking and navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 "use client";
-import { Button, Card, Modal } from "antd";
-import { useState } from "react";
+import { Button, Card } from "antd";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 const Page = () => {
-  const [isOpen, setisOpen] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
   return (
-    <div className="bg-white">
+    <div>
       <Card>
-        <Modal open={isOpen} okType="dashed" onOk={() => setisOpen(false)} />
-        <Button onClick={() => setisOpen(true)}>Click</Button>
+        <ul>
+          <li>
+            <Link
+              className={`Link ${pathname === "/" ? "active" : ""}`}
+              href="/"
+            >
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={`Link ${pathname === "/home" ? "active" : ""}`}
+              href="/home"
+            >
+              Home
+            </Link>
+          </li>
+        </ul>
+        <Button type="text" onClick={() => router.push("/home")}>
+          Change Page
+        </Button>
       </Card>
     </div>
   );
